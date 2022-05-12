11172d8b2ca2cec46d92c9b84561de53bab07746
Goncalo Meda <goncalo@lendigo.ng>
2022-05-12 15:40:59 +0100

    .

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,4 @@
+from django.db.models import OuterRef, Subquery
 from django.views import View
 from django.shortcuts import render
 
@@ -6,18 +7,33 @@ from reservations.models import Reservation
 class IndexView(View):
     def get_context_data(self):
         reservations_list = []
-        for reservation in Reservation.objects.all().order_by('rental__name', 'date_check_in'):
-            previous_reservations = Reservation.objects.filter(
-                rental=reservation.rental,
-                date_check_out__lte=reservation.date_check_in
-            ).exclude(id=reservation.id).order_by('date_check_in')
 
+        previous_reservation = Subquery(
+            Reservation.objects.filter(
+                date_check_out__lte=OuterRef('date_check_in'),
+                rental=OuterRef('rental')
+            )
+            .order_by('-date_check_in')
+            .values('id')[:1]
+        )
+
+        reservations = (
+            Reservation.objects.all()
+            .order_by('rental__name', 'date_check_in')
+            .select_related('rental')
+            .annotate(previous_reservation=previous_reservation)
+        )
+        
+        for reservation in reservations:
             reservations_list.append({
                 'rental_name': reservation.rental.name,
                 'reservation_id': reservation.id,
                 'check_in': str(reservation.date_check_in),
                 'check_out': str(reservation.date_check_out),
-                'previous_reservation': previous_reservations.latest('date_check_in').id if previous_reservations.exists() else '-',
+                'previous_reservation': (
+                    reservation.previous_reservation 
+                    if reservation.previous_reservation else '-'
+                ),
             })
 
         return {
